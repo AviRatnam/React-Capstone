7c859b2332f60e9396f4a68584866c8a3c9cfb30
Avinash Ratnam <54929315+AviRatnam@users.noreply.github.com>
2021-09-20 22:52:40 +0530

    Added delete quiz and quiz topic

--- src/components/PopupQuizForm/PopupQuizForm.jsx
+++ src/components/PopupQuizForm/PopupQuizForm.jsx
@@ -2,11 +2,12 @@ import { useState } from "react";
 import Header from "../Header/Header";
 
 const PopupQuizForm = () => {
-  const quizformstyle = `rounded-lg shadow-lg max-w-full text-gray-700 px-10 py-10 flex-initial flex items-center object-right-top transition `;
+  const quizformstyle = `rounded-lg shadow-lg text-gray-700 px-10 py-10 flex-initial flex items-center object-right-top transition `;
   const borderstyle = `border-2 border-gray-200 rounded-md`;
 
   const [quizName, setQuizName] = useState("");
   const [subjectName, setSubjectName] = useState("");
+  const [topic, setTopic] = useState("");
   const [minutes, setMinutes] = useState(1);
   const [seconds, setSeconds] = useState(1);
   const [questions, setQuestions] = useState(1);
@@ -14,15 +15,25 @@ const PopupQuizForm = () => {
 
   const submitQuiz = (e) => {
     e.preventDefault();
-    const quizDetails = { quizName, subjectName, minutes, seconds, questions, pdf };
-    
-    fetch("http://localhost:8000/quizzes",{
-      method:'POST',
-      headers: {"Content-Type":"application/JSON"},
-      body: JSON.stringify(quizDetails)
-    }).then(()=>{
+    const quizDetails = {
+      quizName,
+      subjectName,
+      topic,
+      minutes,
+      seconds,
+      questions,
+      pdf,
+    };
+
+    //localhost:5000/getext?src=cbse.pdf&dst=ab.txt
+
+    fetch("http://localhost:8000/quizzes", {
+      method: "POST",
+      headers: { "Content-Type": "application/JSON" },
+      body: JSON.stringify(quizDetails),
+    }).then(() => {
       console.log("new quiz added");
-    })
+    });
   };
 
   //npx json-server --watch data/quiz.json --port 8000
@@ -46,6 +57,14 @@ const PopupQuizForm = () => {
           required
           value={subjectName}
           onChange={(e) => setSubjectName(e.target.value)}
+        />
+        <h3>Topic</h3>
+        <input
+          class={borderstyle}
+          type="text"
+          required
+          value={topic}
+          onChange={(e) => setTopic(e.target.value)}
         />
         <h3>Number of Questions</h3>
         <input
@@ -74,10 +93,10 @@ const PopupQuizForm = () => {
           max="60"
           defaultValue={seconds}
           onChange={(e) => setSeconds(e.target.value)}
-        />  
-        <span>Seconds</span> 
+        />
+        <span>Seconds</span>
         <span>Enter pdf</span>
-        <input type="file" onChange={(e)=> setpdf(e.target.value)} />
+        <input type="file" onChange={(e) => setpdf(e.target.value)} />
         <button>Create Quiz</button>
       </form>
     </div>
