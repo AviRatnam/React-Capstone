5ca5cf0d55a04eff3ecb005b3d29a8ffd4caa4f5
Avinash Ratnam <54929315+AviRatnam@users.noreply.github.com>
2021-12-12 14:28:10 +0530

    Changed up profile page

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -6,8 +6,8 @@ const Card = (props) => {
     <div class={cardclass}>
       <a href={props.target}>
         <div class="m-4">
-          <span class="font-bold">{props.title}</span>
-          <span class="block text=gray-500 text-sm">{props.subtitle}</span>
+          <span class="font-bold text-lg">{props.title}</span>
+          <span class="block text-gray-500 text-sm overflow-hidden py-5 px-7">{props.content}</span>
         </div>
         <Time length={props.length} />
         <span>{props.children}</span>
--- src/components/Card/Card.styles.js
+++ src/components/Card/Card.styles.js
@@ -3,6 +3,11 @@ rounded-lg
 overflow-hidden
 shadow-md
 relative
-hover:shadow-lg`;
+hover:shadow-lg
+h-32
+px-5
+py-3
+border-4
+border-white`;
 
 export default cardclass;
